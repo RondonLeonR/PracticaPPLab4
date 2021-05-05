b4bc4351d40a6699fd27c635952fb9ee19cd93fa
RondonLeonR <RondonLeonR@github.com>
2021-05-05 20:44:53 -0300

    linkPag

--- Practica-PP/src/environments/environment.prod.ts
+++ Practica-PP/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCgAQdE92yBDfoIwdydIZewL5oBjiTKsgg",
+    authDomain: "practicalab4-39a4c.firebaseapp.com",
+    projectId: "practicalab4-39a4c",
+    storageBucket: "practicalab4-39a4c.appspot.com",
+    messagingSenderId: "1012263730707",
+    appId: "1:1012263730707:web:8bf09434ebdf954cb3c9e6"
+  }
+  
 };
